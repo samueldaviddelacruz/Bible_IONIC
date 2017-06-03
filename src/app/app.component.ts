60ee6c1801f7be6cf3a6acf60ffdcbb853961da6
Samuel David De La Cruz <alphaelena@gmail.com>
2017-06-02 20:18:17 -0400

    Add basic slide menu Add Antiguo Testamento and Nuevo Testamento options to HomePage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {MenuController, NavController, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -8,9 +8,19 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  homePage:any = HomePage;
+  //rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  @ViewChild('nav')
+  nav:NavController;
+
+
+  onLoad(page: any){
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
+  }
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private menuCtrl:MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +28,6 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
 }
 
